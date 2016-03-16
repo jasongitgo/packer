cdf7bcfe05cf692107338df2c3f095b5f77a0127
jasongitgo <359178hjc>
2016-03-16 22:58:12 +0800

    sss

--- app/components/main/views.py
+++ app/components/main/views.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template,request
 
 from app.components.main import main
 
@@ -8,7 +8,8 @@ from app.components.main import main
 def home():
     return render_template('home.html')
 
-@main.route('/app/new')
-def home():
-    print
+@main.route('/app/new',methods = ['GET', 'POST'])
+def app_new():
+    print(1111)
+    print(request.form)
     return render_template('home.html')
